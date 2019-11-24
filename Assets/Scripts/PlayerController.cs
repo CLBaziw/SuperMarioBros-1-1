5d4fd0fdeb522381580889f72302c499f4b21b04
Christina <cbaziw@outlook.com>
2019-11-24 16:09:19 -0500

    finished map

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    //Public variables
+    public float moveSpeed = 5f;
+    public Vector2 movement;
+    //public Animator animator;
+
+    //Private variables
+    private Rigidbody2D rBody;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rBody = GetComponent<Rigidbody2D>();
+        //animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -18,6 +27,24 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        //Movement
+        //Grabbing movement
+        movement.x = Input.GetAxisRaw("Horizontal");
+        //movement.y = Input.GetAxisRaw("Vertical");
+
+
+
+        if (movement != Vector2.zero)
+        {
+            //Call animator for movement
+            //animator.SetFloat("Horizontal", movement.x);
+            //animator.SetFloat("Vertical", movement.y);
+            //animator.SetLayerWeight(1, 1);
+        }
+        else
+        {
+            //animator.SetLayerWeight(1, 0);
+        }
+
+        rBody.MovePosition(rBody.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
 }
