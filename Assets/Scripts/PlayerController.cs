3229f384f2da00defbc28ee677fe139dd7c1c8f3
Christina <cbaziw@outlook.com>
2019-11-24 20:55:10 -0500

    horizontal movement complete
    working on jumping

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,48 +3,92 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
-{
-    //Public variables
-    public float moveSpeed = 5f;
+{   
+    //Horizontal Movement
     public Vector2 movement;
-    //public Animator animator;
+    public float moveSpeed = 5f;
+    public Animator animator;
 
-    //Private variables
-    private Rigidbody2D rBody;
+    //Jumping
+    public float jumpForce;
+    public float jumpTime;
+    public float jumpTimeCounter;
+    public bool grounded;
+    public LayerMask whatisGround;
+    public bool stoppedJumping;
+    public Transform groundCheck;
+    public float groundCheckRadius;
+
+    private Rigidbody2D rBody; 
 
     // Start is called before the first frame update
     void Start()
     {
         rBody = GetComponent<Rigidbody2D>();
-        //animator = GetComponent<Animator>();
+        animator = GetComponent<Animator>();
+
+        jumpTimeCounter = jumpTime;
     }
 
     // Update is called once per frame
     void Update()
     {
+        movement.x = Input.GetAxisRaw("Horizontal");
+
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatisGround);
         
+        if (grounded)
+        {
+            jumpTimeCounter = jumpTime;
+        }
     }
 
-    private void FixedUpdate()
+    void FixedUpdate()
     {
-        //Grabbing movement
-        movement.x = Input.GetAxisRaw("Horizontal");
-        //movement.y = Input.GetAxisRaw("Vertical");
+        Debug.Log(Input.GetButton("Jump"));
 
-
-
-        if (movement != Vector2.zero)
+        //If you presss the space bar down
+        if (Input.GetButtonDown("Jump"))
         {
-            //Call animator for movement
-            //animator.SetFloat("Horizontal", movement.x);
-            //animator.SetFloat("Vertical", movement.y);
-            //animator.SetLayerWeight(1, 1);
+            if (grounded)
+            {
+                jumpChecker();
+                stoppedJumping = false;
+            }
+        }
+        //If you keep holding the space button
+        if (Input.GetButton("Jump") && !stoppedJumping)
+        {
+            if (jumpTimeCounter > 0)
+            {
+                jumpChecker();
+                jumpTimeCounter -= Time.deltaTime;
+            }
+        }
+        //If you stop holding the space button
+        if (Input.GetButtonUp("Jump"))
+        {
+            jumpTimeCounter = 0;
+            stoppedJumping = true;
+        }
+
+        //Animation for horizontal movement
+        if (movement.x != 0)
+        {
+            animator.SetFloat("Horizontal", movement.x);
+            animator.SetLayerWeight(1, 1);
         }
         else
         {
-            //animator.SetLayerWeight(1, 0);
+            animator.SetLayerWeight(1, 0);
         }
 
+        //Change horizontal position
         rBody.MovePosition(rBody.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    void jumpChecker()
+    {
+        rBody.velocity = new Vector2(rBody.velocity.x, jumpForce);
     }
 }
